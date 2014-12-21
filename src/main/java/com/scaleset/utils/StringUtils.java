c2ca3d9631568a747ff99e577a8c23f91c163bdc
Christopher Lakey <christopherlakey@gmail.com>
2014-12-21 12:36:06 -0500

    migrate useful classes from simple-utils

---
+++ src/main/java/com/scaleset/utils/StringUtils.java
@@ -0,0 +1,84 @@
+package com.scaleset.utils;
+
+public class StringUtils {
+
+
+    /**
+     * Return true if the input is null or empty
+     *
+     * @param str The string to check
+     * @return true if the input is null or empty
+     */
+    public static boolean empty(String str) {
+        return str == null || str.isEmpty();
+    }
+
+    /**
+     * Return true if the input is not empty
+     *
+     * @param str The string to check
+     * @return true only if the input is non-null and not empty
+     */
+    public static boolean notEmpty(String str) {
+        return !empty(str);
+    }
+
+
+    /**
+     * Copied from Spring Framework StringUtils
+     * <p/>
+     * Check whether the given CharSequence has actual text. More specifically,
+     * returns <code>true</code> if the string not <code>null</code>, its length
+     * is greater than 0, and it contains at least one non-whitespace character.
+     * <p/>
+     * <p/>
+     * <pre>
+     * StringUtils.hasText(null) = false
+     * StringUtils.hasText("") = false
+     * StringUtils.hasText(" ") = false
+     * StringUtils.hasText("12345") = true
+     * StringUtils.hasText(" 12345 ") = true
+     * </pre>
+     *
+     * @param str the CharSequence to check (may be <code>null</code>)
+     * @return <code>true</code> if the CharSequence is not <code>null</code>,
+     * its length is greater than 0, and it does not contain whitespace
+     * only
+     * @see java.lang.Character#isWhitespace
+     */
+    public static boolean hasText(CharSequence str) {
+        if (!hasLength(str)) {
+            return false;
+        }
+        int strLen = str.length();
+        for (int i = 0; i < strLen; i++) {
+            if (!Character.isWhitespace(str.charAt(i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Copied from Spring Framework StringUtils
+     * <p/>
+     * Check that the given CharSequence is neither <code>null</code> nor of
+     * length 0. Note: Will return <code>true</code> for a CharSequence that
+     * purely consists of whitespace.
+     * <p/>
+     * <p/>
+     * <pre>
+     * StringUtils.hasLength(null) = false
+     * StringUtils.hasLength("") = false
+     * StringUtils.hasLength(" ") = true
+     * StringUtils.hasLength("Hello") = true
+     * </pre>
+     *
+     * @param str the CharSequence to check (may be <code>null</code>)
+     * @return <code>true</code> if the CharSequence is not null and has length
+     * @see #hasText(CharSequence)
+     */
+    public static boolean hasLength(CharSequence str) {
+        return (str != null && str.length() > 0);
+    }
+}
